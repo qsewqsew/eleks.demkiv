ea68a0370841f123bc45c378d1f0b382f5ab541d
Kseniya Demkiv <kseniya.demkiv_tac@academy.eleks.local>
2017-06-13 18:46:45 +0300

    new commit

---
+++ maven.learn/src/test/java/eleks/AppTest.java
@@ -0,0 +1,16 @@
+package eleks;
+
+import org.testng.annotations.Test;
+import org.testng.Assert;
+
+public class AppTest
+
+{
+    @Test
+    public void testDivision() {
+        Assert.assertEquals(2, 10 / 5);
+    }
+
+
+
+}
